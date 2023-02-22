37c5dd432f5123bb98331717e698af590b72c259
Sanghun Jo <salr921@gmail.com>
2023-02-22 11:22:39 +0900

    basic django rest project

---
+++ tutorial_rest/quickstart/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from tutorial_rest.quickstart.serializers import UserSerializer, GroupSerializer
+# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all().order_by('-date_joined')
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
